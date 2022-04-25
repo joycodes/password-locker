c9736f464937ed429493f7c65e526c285a815ad1
Joyce Nguttu <jnguttu@gmail.com>
2022-04-25 03:50:14 +0300

    replace inputs in user_test

--- credentials.py
+++ credentials.py
@@ -57,19 +57,3 @@ class Credentials:
         return cls.credentials_list
 
     # pyperclip.copy(credentials_located.account_password)
-Credentials.credentials_list.append(self) 
-        Credentials.credentials_list.append(self)
-
-    @classmethod   
-    def find_credentials_by_username(cls,username):
-        '''
-        method to search a credentials in credentials_list by the username
-        
-        arg:
-            cls is the to make the class an argument and username to take in 
-            a username as the argument
-        '''
-
-        for credential in cls.credentials_list:
-            if credential.username_cred == username:
-                return credential
